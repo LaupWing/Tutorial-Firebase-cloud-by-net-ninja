242e62809e1761f927483d971c0bc3a625e94e95
LaupWing <loc_strict@hotmail.com>
2020-04-16 17:18:03 +0200

    6.5

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,12 @@
 const requestModal = document.querySelector('.new-request')
 const requestLink = document.querySelector('.add-request')
 
-
-requestLink.addEventListener('click', ()=>{
+requestLink.addEventListener('click', () => {
     requestModal.classList.add('open')
 })
 
-requestModal.addEventListener('click', e =>{
-    if(e.target.classList.contains('new-request')){
+requestModal.addEventListener('click', e => {
+    if (e.target.classList.contains('new-request')) {
         requestModal.classList.remove('open')
     }
 })
--- public/js/auth.js
+++ public/js/auth.js
@@ -1,11 +1,26 @@
 const switches = document.querySelectorAll('.switch')
 const authModal = document.querySelectorAll('.auth .modal')
 const authWrapper = document.querySelector('.auth')
-
+const registerForm = document.querySelector('.register')
+const loginForm = document.querySelector('.login')
+const signOut = document.querySelector('.sign-out')
 // Toggle Auth
-switches.forEach(link=>{
-    link.addEventListener('click', e=>{
+switches.forEach(link => {
+    link.addEventListener('click', e => {
         e.preventDefault()
-        authModal.forEach(modal=>modal.classList.toggle('active'))
+        authModal.forEach(modal => modal.classList.toggle('active'))
     })
+})
+
+registerForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const email = registerForm.email.value
+    const password = registerForm.password.value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(user => {
+            console.log(user)
+        })
+        .catch(error => {
+            registerForm.querySelector('p.error').textContent = error.message
+        })
 })
