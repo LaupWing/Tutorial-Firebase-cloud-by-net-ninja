b7082298ca69c917d42483d83901c3493bcc0f31
LaupWing <loc_strict@hotmail.com>
2020-04-17 14:17:35 +0200

    ;

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,11 @@ const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
 
 requestLink.addEventListener('click', () => {
-    requestModal.classList.add('open')
+    requestModal.classList.add('open');
 });
 
 requestModal.addEventListener('click', e => {
     if (e.target.classList.contains('new-request')) {
-        requestModal.classList.remove('open')
+        requestModal.classList.remove('open');
     }
 });
