15e067fa26d2ec69ea1432f0b640fe662c82b291
LaupWing <loc_strict@hotmail.com>
2020-04-20 17:37:04 +0200

    13

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,9 +1,15 @@
-const ref = firebase.firestore().collection('requests');
-
-ref.onSnapshot(snapshot=>{
-    let requests = [];
-    snapshot.forEach(doc=>{
-        requests.push({...doc.data(), id: doc.id});
-    });
-    console.log(requests)
-});
+const app = new Vue({
+    el: '#app',
+    data:{
+        requests:[]
+    },
+    mounted(){
+        const ref = firebase.firestore().collection('requests');
+        ref.onSnapshot(snapshot=>{
+            snapshot.forEach(doc=>{
+                this.requests.push({...doc.data(), id: doc.id});
+            });
+            console.log(this.requests)
+        });
+    }
+})
