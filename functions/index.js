2ea3470a618cf18f44e524a0da6e6dd3cba46070
LaupWing <loc_strict@hotmail.com>
2020-04-21 14:30:31 +0200

    14

--- functions/index.js
+++ functions/index.js
@@ -35,3 +35,37 @@ exports.addRequest = functions.https.onCall((data, context)=>{
         upvotes: 0
     });
 });
+
+
+// UPvtoe callable function
+
+exports.upvote = functions.https.onCall((data, context)=>{
+    // Check auth state
+    if(!context.auth){
+        throw new functions.https.HttpsError(
+            'unauthenticated', 
+            'Only authenticated users can add request'
+        );
+    }
+    // Get Refs for users doc & requests doc
+    const user = admin.firestore().collection('users').doc(context.auth.uid);
+    const request = admin.firestore().collection('requests').doc(data.id);
+    return user.get().then(doc=>{
+        // Check user hasnt already upvoted rerquest
+        if(doc.data().upvotedOn.includes(data.id)){
+            throw new functions.https.HttpsError(
+                'failed-precondition', 
+                'You can only upvote something once'
+            );
+        }
+        // update user array
+        return user.update({
+            upvotedOn:[...doc.data().upvotedOn, data.id]
+        }).then(()=>{
+            // update votes on request
+            return request.update({
+                upvotes: admin.firestore.FieldValue.increment(1);
+            })
+        });
+    })
+})
